31c7624100f58c4cd131a1d10920d3a795417e4d
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-07-22 04:42:20 -0400

    Make TicTacToe table unstackable

--- src/components/TicTacToeTable.js
+++ src/components/TicTacToeTable.js
@@ -24,7 +24,7 @@ export default class TicTacToeTable extends Component {
 
   render = () =>
     <Container>
-      <Table fixed celled style={this.state.style}>
+      <Table fixed unstackable celled style={this.state.style}>
         <Table.Body>
           {
             this.state.table.map(row =>
