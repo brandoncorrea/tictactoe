88e00cd1effbe98f54103edb123171769cb71e45
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-07-22 03:43:35 -0400

    Refactor Score

--- src/components/Score.js
+++ src/components/Score.js
@@ -3,18 +3,13 @@ import { Statistic } from "semantic-ui-react";
 import ScoreRepository from "../data/ScoreRepository";
 
 export default class Score extends Component {
-  constructor(props) {
-    super(props);
-    var score = new ScoreRepository();
-    this.state = {
-      items: [
-        { key: 'wins', label: 'Wins', value: score.getWins().toLocaleString() },
-        { key: 'draws', label: 'Draws', value: score.getDraws().toLocaleString() },
-        { key: 'losses', label: 'Losses', value: score.getLosses().toLocaleString() },
-      ]
-    }
-  }
+  score = new ScoreRepository();
+  items = [
+    { key: 'wins', label: 'Wins', value: this.score.getWins().toLocaleString() },
+    { key: 'draws', label: 'Draws', value: this.score.getDraws().toLocaleString() },
+    { key: 'losses', label: 'Losses', value: this.score.getLosses().toLocaleString() },
+  ]
 
   render = () => 
-    <Statistic.Group size='huge' horizontal items={this.state.items}/>
+    <Statistic.Group size='huge' horizontal items={this.items}/>
 }
