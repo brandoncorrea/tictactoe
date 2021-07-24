3dc21cc13dc1061efb3b1b0d695c0920106dc60e
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-07-24 17:00:45 -0400

    Update Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,8 +2,10 @@ import { Component } from "react";
 import { Container } from "semantic-ui-react";
 import ScoreRepository from "../data/ScoreRepository";
 import SettingsRepository from "../data/SettingsRepository";
+import { FirstPlayer } from "../enums/FirstPlayer";
 import { GameResult } from "../enums/GameResult";
 import GameBoard from "../game/GameBoard";
+import GameBoardAnalyzer from "../helpers/GameBoardAnalyzer";
 import TicTacToeTable from "./TicTacToeTable";
 
 export default class Game extends Component {
@@ -11,24 +13,48 @@ export default class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      game: new GameBoard()
+      game: new GameBoard(),
+      analyzer: new GameBoardAnalyzer(),
     }
 
     this.settings = new SettingsRepository();
     this.score = new ScoreRepository();
     this.onCellClick = this.onCellClick.bind(this);
+    if (this.settings.getFirstPlayer() === FirstPlayer.Computer)
+      this.playComputerTurn();
+  }
+
+  gameOver = () =>
+    this.state.game.getGameResult(this.settings.getUserIcon()) !== GameResult.None
+
+  playComputerTurn() {
+    var userToken = this.settings.getUserIcon();
+    var computerToken = this.settings.getComputerIcon();
+    var cell = this.state.analyzer.getOptimalCell(
+      this.state.game, 
+      computerToken, 
+      userToken)
+
+    this.state.game.placeToken(computerToken, cell);
+    this.setState({ });
   }
 
   onCellClick(cell) {
-    var token = this.settings.getUserIcon();
-    if (this.state.game.getGameResult(token) !== GameResult.None)
-      return;
+    if (this.gameOver()) return;
 
-    this.state.game.placeToken(token, cell);
-    this.setState({ })
+    // Play user's turn
+    var userToken = this.settings.getUserIcon();
+    this.state.game.placeToken(userToken, cell);
+    this.setState({ });
+
+    // Play computer's turn
+    if (!this.gameOver())
+      this.playComputerTurn()
+      
+    this.setState({ });
 
     // Update score
-    var result = this.state.game.getGameResult(token);
+    var result = this.state.game.getGameResult(userToken);
     if (result === GameResult.Draw)
       this.score.addDraw();
     else if (result === GameResult.Loss)
