ed9f4e89f5fb232b24519b8c5b3056becf25d24c
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-07-26 01:15:59 -0400

    Update settings

--- src/components/FirstPlayerSetting.js
+++ src/components/FirstPlayerSetting.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Form, Radio } from 'semantic-ui-react';
+import { Form, Header, Radio } from 'semantic-ui-react';
 import SettingsRepository from "../data/SettingsRepository";
 import { Players } from "../enums/Players";
 
@@ -21,21 +21,23 @@ export default class FirstPlayerSetting extends Component {
   }
 
   render = () => 
-  <Form.Group inline>
-    <label>Who goes first?</label>
-    <Form.Field
-      control={Radio}
-      label="I'll go first"
-      value={Players.Player1}
-      checked={this.state.value === Players.Player1}
-      onChange={this.handleChange}
-      />
-    <Form.Field
-      control={Radio}
-      label='Opponent will go first'
-      value={Players.Player2}
-      checked={this.state.value === Players.Player2}
-      onChange={this.handleChange}
-      />
-  </Form.Group>
+  <Form>
+    <Header as='h3' content='Who goes first?' />
+    <Form.Group widths='equal'>
+      <Form.Field
+        control={Radio}
+        label="I'll go first"
+        value={Players.Player1}
+        checked={this.state.value === Players.Player1}
+        onChange={this.handleChange}
+        />
+      <Form.Field
+        control={Radio}
+        label='Opponent will go first'
+        value={Players.Player2}
+        checked={this.state.value === Players.Player2}
+        onChange={this.handleChange}
+        />
+    </Form.Group>
+  </Form>
 }
