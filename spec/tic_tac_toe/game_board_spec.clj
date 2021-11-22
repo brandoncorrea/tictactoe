f477b0b92961a8a0fe3365cba01cd8b6ce5695fc
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-11-22 12:25:20 -0500

    Rename empty-board

--- spec/tic_tac_toe/game_board_spec.clj
+++ spec/tic_tac_toe/game_board_spec.clj
@@ -2,16 +2,10 @@
   (:require [speclj.core :refer :all]
             [tic-tac-toe.game-board :refer :all]))
 
-(def empty-board
+(def empty-3x3
   {[0 0] nil [0 1] nil [0 2] nil
    [1 0] nil [1 1] nil [1 2] nil
    [2 0] nil [2 1] nil [2 2] nil})
-
-(def empty-4x4
-  {[0 0] nil [0 1] nil [0 2] nil [0 3] nil
-   [1 0] nil [1 1] nil [1 2] nil [1 3] nil
-   [2 0] nil [2 1] nil [2 2] nil [2 3] nil
-   [3 0] nil [3 1] nil [3 2] nil [3 3] nil})
 
 (def empty-3x3x3
   {[0 0 0] nil [0 0 1] nil [0 0 2] nil
@@ -23,6 +17,12 @@
    [2 0 0] nil [2 0 1] nil [2 0 2] nil
    [2 1 0] nil [2 1 1] nil [2 1 2] nil
    [2 2 0] nil [2 2 1] nil [2 2 2] nil})
+
+(def empty-4x4
+  {[0 0] nil [0 1] nil [0 2] nil [0 3] nil
+   [1 0] nil [1 1] nil [1 2] nil [1 3] nil
+   [2 0] nil [2 1] nil [2 2] nil [2 3] nil
+   [3 0] nil [3 1] nil [3 2] nil [3 3] nil})
 
 (def win-result {:draw false :game-over true :winner 1})
 (def draw-result {:draw true :game-over true :winner nil})
@@ -36,30 +36,30 @@
 (describe "game-board"
   (describe "->board"
     (it "Partitions 3x3 empty board"
-      (should= empty-board (->board (repeat nil))))
+      (should= empty-3x3 (->board (repeat nil))))
     (it "Partitions 3x3 board with all 1s"
       (should= {[0 0] 1 [0 1] 1 [0 2] 1
                 [1 0] 1 [1 1] 1 [1 2] 1
                 [2 0] 1 [2 1] 1 [2 2] 1}
                (->board (repeat 1))))
     (it "No cells results in an empty map"
-      (should= empty-board (->board [])))
+      (should= empty-3x3 (->board [])))
     (it "Can be initialized with one cell"
-      (should= (assoc empty-board [0 0] 1) (->board [1])))
+      (should= (assoc empty-3x3 [0 0] 1) (->board [1])))
     (it "Can be initialized with two cells"
-      (should= (merge empty-board {[0 0] 1 [0 1] 2})
+      (should= (merge empty-3x3 {[0 0] 1 [0 1] 2})
                (->board [1 2])))
     (it "Can be initialized with three cells"
-      (should= (merge empty-board {[0 0] 1 [0 1] 2 [0 2] 3})
+      (should= (merge empty-3x3 {[0 0] 1 [0 1] 2 [0 2] 3})
                (->board [1 2 3])))
     (it "Can be initialized with four cells"
-      (should= (merge empty-board {[0 0] 1 [0 1] 2 [0 2] 3 [1 0] 4})
+      (should= (merge empty-3x3 {[0 0] 1 [0 1] 2 [0 2] 3 [1 0] 4})
                (->board (range 1 5))))
     (it "Can be initialized with five cells"
-      (should= (merge empty-board {[0 0] 1 [0 1] 2 [0 2] 3 [1 0] 4 [1 1] 5})
+      (should= (merge empty-3x3 {[0 0] 1 [0 1] 2 [0 2] 3 [1 0] 4 [1 1] 5})
                (->board (range 1 6))))
     (it "Can be initialized with six cells"
-      (should= (merge empty-board {[0 0] 1 [0 1] 2 [0 2] 3 [1 0] 4 [1 1] 5 [1 2] 6})
+      (should= (merge empty-3x3 {[0 0] 1 [0 1] 2 [0 2] 3 [1 0] 4 [1 1] 5 [1 2] 6})
                (->board (range 1 7))))
     (it "Can be initialized with full board"
       (should= {[0 0] 1 [0 1] 2 [0 2] 3 [1 0] 4 [1 1] 5 [1 2] 6 [2 0] 7 [2 1] 8 [2 2] 9}
@@ -76,10 +76,10 @@
   (describe "mark-square"
     (it "Player 1 marks the first square"
       (should= (->board [1])
-               (mark-square empty-board [0 0] 1)))
+               (mark-square empty-3x3 [0 0] 1)))
     (it "Player 2 marks the second square"
       (should= (->board [nil 2])
-               (mark-square empty-board [0 1] 2)))
+               (mark-square empty-3x3 [0 1] 2)))
     (it "Marks 4x4 board"
       (should= (merge empty-4x4 {[0 2] 2})
                (mark-square empty-4x4 [0 2] 2)))
@@ -134,7 +134,7 @@
 
   (describe "full-board?"
     (it "Empty board results in false"
-      (should= false (full-board? empty-board)))
+      (should= false (full-board? empty-3x3)))
     (it "Results in false when board is full except one cell"
       (should= false (full-board? (->board (range 8)))))
     (it "Results in true when there are no nil values"
@@ -148,7 +148,7 @@
 
   (describe "game-results"
     (it "Empty game board results in game not over"
-      (should= game-not-over-result (game-results empty-board)))
+      (should= game-not-over-result (game-results empty-3x3)))
     (it "Board with one filled square results in game not over"
       (should= game-not-over-result (game-results (->board [1]))))
     (it "Board with first three items as 1 results in a win"
@@ -174,21 +174,21 @@
 
   (describe "valid-move?"
     (it "Results in true for empty cell"
-      (should= true (valid-move? empty-board [0 0])))
+      (should= true (valid-move? empty-3x3 [0 0])))
     (it "Results in false when first cell is occupied"
-      (should= false (valid-move? (mark-square empty-board [0 0] 1) [0 0])))
+      (should= false (valid-move? (mark-square empty-3x3 [0 0] 1) [0 0])))
     (it "Results in false when last cell is occupied"
-      (should= false (valid-move? (mark-square empty-board [2 2] 1) [2 2])))
+      (should= false (valid-move? (mark-square empty-3x3 [2 2] 1) [2 2])))
     (it "Invalid moves result in false"
       (loop [[move & rest-moves] invalid-moves]
         (when move
-          (should= false (valid-move? empty-board move))
+          (should= false (valid-move? empty-3x3 move))
           (recur rest-moves))))
     (it "Valid positions result in true for empty spaces and false for occupied spaces"
       (loop [[move & rest-moves] valid-moves]
         (when move
-          (should= true (valid-move? empty-board move))
-          (should= false (valid-move? (mark-square empty-board move 1) move))
+          (should= true (valid-move? empty-3x3 move))
+          (should= false (valid-move? (mark-square empty-3x3 move 1) move))
           (recur rest-moves))))
     (it "[0 0] results in false for empty vector"
       (should= false (valid-move? [] [0 0]))))
