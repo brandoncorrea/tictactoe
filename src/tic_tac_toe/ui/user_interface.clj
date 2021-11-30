98b7f7f142ea7786fca7a95a3cee72ad1e49961e
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-11-30 01:12:59 -0500

    Delete commented code

--- src/tic_tac_toe/ui/user_interface.clj
+++ src/tic_tac_toe/ui/user_interface.clj
@@ -1,15 +1,5 @@
 (ns tic-tac-toe.ui.user-interface)
 
-;(defprotocol UserInterface
-;  (show-title [this])
-;  (show-instructions [this])
-;  (show-results [this board])
-;  (request-game-mode [this])
-;  (request-board-size [this])
-;  (request-board-dimensions [this size])
-;  (request-difficulty [this])
-;  (request-move [this board player]))
-
 (defn- type-of [m & _] (:type m))
 (defmulti show-title type-of)
 (defmulti show-instructions type-of)
