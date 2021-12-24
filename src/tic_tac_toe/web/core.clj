ba0a3936548f0c8c8cfef53b37b31ddcbf978d94
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-12-24 05:04:22 -0500

    Add routes to main

--- src/tic_tac_toe/web/core.clj
+++ src/tic_tac_toe/web/core.clj
@@ -2,11 +2,13 @@
   (:require [http-server.core :as h]
             [tic-tac-toe.data.datomic-db :as datomic-db]
             [tic-tac-toe.web.routes.index :as index]
+            [tic-tac-toe.web.routes.move :as move]
             [tic-tac-toe.web.routes.new-game :as new-game]))
 
 (defn create-routes [db]
   {:/ {:get (partial index/render db)}
-   :new-game {:post (partial new-game/render db)}})
+   :new-game {:post (partial new-game/render db)}
+   :move {:post (partial move/render db)}})
 
 (defn -main []
   (println "Connecting to datomic...")
