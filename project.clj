bc2cf4454e44eb93d75b57e8f7c880f39d492706
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2022-01-06 08:43:59 -0500

    First commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject org.clojars.brandoncorrea/ttt-data "0.1.0-SNAPSHOT"
+  :description "Tic Tac Toe datomic interface"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main ttt-data.core
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojars.brandoncorrea/ttt-core "0.1.0-SNAPSHOT"]
+                 [com.datomic/datomic-free "0.9.5697"]]
+  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
+  :plugins [[speclj "3.3.2"]]
+  :test-paths ["spec"])
